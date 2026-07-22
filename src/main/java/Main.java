14dfcb5ef9524d26be6ae0143931adbbcd81a4ef
Yomna Magdy <yomnaaaa2@gmail.com>
2026-07-22 22:07:08 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
+      while(true){
         System.out.print("$ ");
         Scanner scanner = new Scanner (System.in);
         String command = scanner.nextLine();
         System.out.println(command + ": command not found");
+       }
     }
 }
