e5a18b9955371a5de1e9afa69449f63d18c1e384
Yomna Magdy <yomnaaaa2@gmail.com>
2026-07-24 18:19:45 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
-      while(true){
-        System.out.print("$ ");
         Scanner scanner = new Scanner (System.in);
+     while(true){
+        System.out.print("$ ");
         String command = scanner.nextLine();
-        if (input.equals("exit")){
+        if (command.equals("exit")){
         break;
         }
         System.out.println(command + ": command not found");
