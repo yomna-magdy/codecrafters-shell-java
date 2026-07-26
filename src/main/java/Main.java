3dd400c0f7c45845548bec845b73e362cdf01ada
Yomna Magdy <yomnaaaa2@gmail.com>
2026-07-26 13:00:22 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,20 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
-        Scanner scanner = new Scanner (System.in);
-     while(true){
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+else if (command.startsWith("type")) {
+                System.out.println(command.substring(5) + " is a shell builtin");
+            }
+            System.out.print("$ ");
+            String command = scanner.nextLine();
 
-        System.out.print("$ ");
-        String command = scanner.nextLine();
-        if (command.equals("exit")){
-        break;
-        } else if(command.startsWith("echo")){
-            System.out.println(command.substring(5));
-        } else if (command.startsWith("type")) {
-            System.out.println(command.substring(5) + " is a shell builtin");
-        } else {
-            System.out.println(command + ": command not found");
-             }
-     }
+            if (command.equals("exit")) {
+                break;
+            } else if (command.startsWith("echo")) {
+                System.out.println(command.substring(5));
+            } else if(command.startsWith("type") && command == "echo" || command == "exit"){
+                System.out.println(command.substring(5) + " is a shell builtin");
+            }
+            else {
+                System.out.println(command + ": command not found");
+
+            }
+        }
     }
-}
+
+
