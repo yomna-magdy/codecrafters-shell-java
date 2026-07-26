8f82dc2a52c6953d28607817ba12ae02961d9d2e
Yomna Magdy <yomnaaaa2@gmail.com>
2026-07-26 12:51:51 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,16 @@ public class Main {
         // TODO: Uncomment the code below to pass the first stage
         Scanner scanner = new Scanner (System.in);
      while(true){
+
         System.out.print("$ ");
         String command = scanner.nextLine();
         if (command.equals("exit")){
         break;
-        }
-        else if(command.startsWith("echo")){
+        } else if(command.startsWith("echo")){
             System.out.println(command.substring(5));
-        }
-        else {
+        } else if (command.startsWith("type")) {
+            System.out.println(command.substring(4) + "is a shell builtin");
+        } else {
             System.out.println(command + ": command not found");
              }
      }
