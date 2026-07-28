9c1d8460e3a6a940841b88ce222e206f3d2199b3
Yomna Magdy <yomnaaaa2@gmail.com>
2026-07-28 12:14:42 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,7 +25,7 @@ public class Main {
         else if (command.startsWith("type")) {
 
             //if the command equals to a builtin command
-            if(command.equals("type exit")  || command.equals("type echo") ){
+            if(command.equals("type exit")  || command.equals("type echo") || command.equals("type type")){
             System.out.println(command.substring(5) + " is a shell builtin");}
 
             //if the command doesn't equal a builtin command
