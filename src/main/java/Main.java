a80b950283bf0c15a5d079cdc90e33df77a51164
Yomna Magdy <yomnaaaa2@gmail.com>
2026-07-28 12:17:56 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,14 +25,15 @@ public class Main {
         else if (command.startsWith("type")) {
 
             //if the command equals to a builtin command
-            if(command.equals("type exit")  || command.equals("type echo") || command.equals("type type")){
-            System.out.println(command.substring(5) + " is a shell builtin");}
-
-            //if the command doesn't equal a builtin command
-            else {
-                System.out.println(command.substring(5) + ": not found");
+            if (command.equals("type exit") || command.equals("type echo") || command.equals("type type")) {
+                System.out.println(command.substring(5) + " is a shell builtin");
             }
         }
+            //if the command doesn't equal a builtin command
+        else {
+                System.out.println(command.substring(5) + " command not found");
+            }
+
      }
     }
 }
